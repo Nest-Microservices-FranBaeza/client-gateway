1db02cd511d20be9d9d05513846f6c1c3cbda642
Franco Baeza <francobaezagraf@gmail.com>
2024-07-17 20:33:46 -0300

    done prods microservice

---
+++ src/products/products.module.ts
@@ -0,0 +1,22 @@
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { Module } from '@nestjs/common';
+
+import { ProductsController } from './products.controller';
+import { envs, PRODUCT_SERVICE } from 'src/config';
+
+@Module({
+    controllers: [ProductsController],
+    imports: [
+        ClientsModule.register([
+            {
+                name: PRODUCT_SERVICE,
+                transport: Transport.TCP,
+                options: {
+                    host: envs.productsMicroserviceHost,
+                    port: envs.productsMicroservicePort,
+                },
+            },
+        ]),
+    ],
+})
+export class ProductsModule {}
