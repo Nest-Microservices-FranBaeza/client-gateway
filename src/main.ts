93dc5adf344b0025cbb23ae61296dc309526f0f3
Franco Baeza <francobaezagraf@gmail.com>
2024-08-01 16:48:13 -0300

    chore: Add health check module and controller

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
     const logger = new Logger('Main-Gateway');
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('api');
+    app.setGlobalPrefix('api', {
+        exclude: [
+            {
+                path: '',
+                method: RequestMethod.GET,
+            },
+        ],
+    });
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
@@ -17,6 +24,6 @@ async function bootstrap() {
     app.useGlobalFilters(new RpcCustomExceptionFilter());
     await app.listen(envs.port);
     logger.log(`Gateway is running on: ${await app.getUrl()}`);
-    console.log('cabmios')
+    console.log('cabmios');
 }
 bootstrap();
